b12b76cc4de211df0fad5a9d883affd3e5e0d82a
Alexandre MONTORO <alexandre.montoro@viacesi.fr>
2017-09-28 09:34:06 +0200

    Initialisation

--- Parseur v1/Parseur v1/Program.cs
+++ Parseur v1/Parseur v1/Program.cs
@@ -14,6 +14,7 @@ namespace Parseur_v1
         [STAThread]
         static void Main()
         {
+            Console.Write("Parseur V1");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
