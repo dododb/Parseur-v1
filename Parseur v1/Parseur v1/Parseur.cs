29c37414fcf2f10987ea0610799ccbb0bb457e64
dododb <dorian.bataille@viacesi.fr>
2017-09-29 20:34:46 +0200

    Ajout de la fonctionnalité qui permet de crée le parseur a l'aide des element surligné par l'utilisateur graphiquement

--- Parseur v1/Parseur v1/Parseur.cs
+++ Parseur v1/Parseur v1/Parseur.cs
@@ -10,12 +10,12 @@ namespace Parseur_v1
 {
     class Parseur
     {
-        private string patternInitial;
+        private string[] separators;
         private string patternFinal;
 
-        public Parseur(string patternInitial, string patternFinal)
+        public Parseur(string[] patternInitial, string patternFinal)
         {
-            this.patternInitial = patternInitial;
+            this.separators = patternInitial;
             this.patternFinal = patternFinal;
         }
 
@@ -26,80 +26,64 @@ namespace Parseur_v1
 
         public static Parseur CreateNewParseur(string initial, Dictionary<Color, Tuple<int, string>> elementsInitial, string final, Dictionary<Color, Tuple<int, string>> elementsFinal)
         {
-            string patternInit = initial;
+            string[] patternInit;
             string patternFinal = final;
 
+            patternInit = initial.Split(elementsInitial.AsEnumerable().Select(x => x.Value.Item2).ToArray(), StringSplitOptions.RemoveEmptyEntries);//.Where(x => x.Length > 0).ToArray();
+            
+            //sous la forme index, length
+            List<Tuple<int, int>> decalage = new List<Tuple<int, int>>();
+
             int i = 0;
-            foreach(var elementInitial in elementsInitial)
+            foreach (var elementInitial in elementsInitial)
             {
                 string coloredString = elementInitial.Value.Item2;
+                
+                //replace the current word by {X} (where X is a number) in patternFinal
 
-                //replace the current word by <varX> (where X is a number) in both patternInitial and patternFinal
                 if (elementsFinal.TryGetValue(elementInitial.Key, out Tuple<int, string> elementFinal))
                 {
-                    int indexInit = elementInitial.Value.Item1;
-                    int indexFinal = elementFinal.Item1;
+                    //int indexInit = elementInitial.Value.Item1;
+                    int indexFinal = elementFinal.Item1 - (from decal in decalage
+                                                          where decal.Item1 < elementFinal.Item1
+                                                          select decal.Item2 - (2 + i.ToString().Length)).Sum();
 
-                    patternInit = patternInit.Take(indexInit - 1) + "<var" + i + ">" + new string(patternInit.Skip(indexInit + coloredString.Length).ToArray());
-                    patternInit = patternInit.Take(indexInit - 1) + "<var" + i + ">" + new string(patternInit.Skip(indexInit + coloredString.Length).ToArray());
-
-                    patternFinal = patternFinal.Take(indexFinal - 1) + "<var" + i + ">" + new string(patternFinal.Skip(indexFinal + coloredString.Length).ToArray());
-                    patternFinal = patternFinal.Take(indexFinal - 1) + "<var" + i + ">" + new string(patternFinal.Skip(indexFinal + coloredString.Length).ToArray());
-
+                    patternFinal = new string(patternFinal.Take(indexFinal).ToArray()) + "{" + i + "}" + new string(patternFinal.Skip(indexFinal + coloredString.Length).ToArray());
+                    decalage.Add(new Tuple<int, int>(elementFinal.Item1, coloredString.Length));
                     i++;
                 }
             }
-            Console.WriteLine("initial : " + patternInit);
-            Console.WriteLine("final   : " + patternFinal);
+            //Console.WriteLine("initial : " + patternInit);
+            //Console.WriteLine("final   : " + patternFinal);
             return new Parseur(patternInit, patternFinal); ;
         }
 
         public string DoParsing(string stringToParse)
         {
-            //get all separator in the string order
-            var separators = from separator in Regex.Split(this.patternInitial, "<var[0-9]+>")
-                                  where separator.Length > 0
-                                  select separator;
-
             //recupere tout les mots qui sont des variables
             List < string > variables = new List<string>();
-            foreach(string separator in separators)
-            {
-                var splited = Regex.Split(stringToParse, separator);
+            variables = stringToParse.Split(this.separators, StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length > 0).ToList();
 
-                //if the split suceede
-                if (splited.Length > 1)
-                {
-                    if (splited[0].Length > 0)
-                    {
-                        variables.Add(splited[0]);
-                        stringToParse = splited[1];
-                    }
-                    else
-                    {
-                        stringToParse = splited[1];
-                    }
-                }
-            }
-            //when string to parse is not empty, the staying is the last variable
-            variables.Add(stringToParse);
-
-            //replace variable with patternFinal
-            string result = patternFinal;
-            int i = 0;
-            foreach(string variable in variables)
-            {
-                string toReplace = "<var" + i + ">";
-                result = result.Replace(toReplace, variable);
-                i++;
-            }
-            return result;
+            return string.Format(patternFinal, variables.ToArray()); // Regex.Replace(result, "<var[0-9]*>", "");
         }
 
         public static void Test()
         {
-            Parseur a = new Parseur(",<var0>:<var1>;;<var2>", "<var1>\n<var0>\n<var2>");
-            Console.WriteLine(a.DoParsing(",papa:maman;;soeur"));
+            string initial= ",{0}mm;;soeur//toto";
+            Dictionary<Color, Tuple<int, string>> elementsInitial = new Dictionary<Color, Tuple<int, string>>();
+            elementsInitial.Add(Color.Black, new Tuple<int, string>(1, "{0}mm"));
+            elementsInitial.Add(Color.AliceBlue, new Tuple<int, string>(8, "soeur"));
+            elementsInitial.Add(Color.AntiqueWhite, new Tuple<int, string>(15, "toto"));
+
+            string final = "soeur\n{0}mm\ntoto";
+            Dictionary<Color, Tuple<int, string>> elementsFinal = new Dictionary<Color, Tuple<int, string>>();
+            elementsFinal.Add(Color.Black, new Tuple<int, string>(6, "{0}mm"));
+            elementsFinal.Add(Color.AliceBlue, new Tuple<int, string>(0, "soeur"));
+            elementsFinal.Add(Color.AntiqueWhite, new Tuple<int, string>(12, "toto"));
+
+            var a = CreateNewParseur(initial, elementsInitial, final, elementsFinal);
+            //Parseur a = new Parseur(new string[]{ ",", ":", ";;", "ii" }, "{0}\n{1}\n{2}\n{3}");
+            Console.WriteLine(a.DoParsing(",maman;;soeur//papa"));
         }
     }
 }
