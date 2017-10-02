a3fb2731bcd678f606c6617d769bbe9304e6ec32
dododb <dorian.bataille@viacesi.fr>
2017-10-02 13:49:58 +0200

    Finalisation Parseur

--- Parseur v1/Parseur v1/Form1.Designer.cs
+++ Parseur v1/Parseur v1/Form1.Designer.cs
@@ -35,12 +35,12 @@
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
@@ -51,9 +51,9 @@
             this.tableLayoutPanel1.ColumnCount = 5;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.49096F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.50904F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 421F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 438F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 561F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 584F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 28F));
             this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 3, 1);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
@@ -62,22 +62,24 @@
             this.tableLayoutPanel1.Controls.Add(this.button7, 3, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.977273F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 96.02273F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1040, 505);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1387, 622);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // richTextBox2
             // 
             this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(162, 20);
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(217, 26);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(415, 427);
+            this.richTextBox2.Size = new System.Drawing.Size(553, 524);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             this.richTextBox2.SelectionChanged += new System.EventHandler(this.RichTextBox2_SelectionChanged);
@@ -86,10 +88,11 @@
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(583, 20);
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(778, 26);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(432, 427);
+            this.richTextBox1.Size = new System.Drawing.Size(576, 524);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             this.richTextBox1.SelectionChanged += new System.EventHandler(this.RichTextBox1_SelectionChanged);
@@ -106,39 +109,42 @@
             this.tableLayoutPanel2.Controls.Add(this.button4, 0, 6);
             this.tableLayoutPanel2.Controls.Add(this.button5, 0, 7);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(24, 20);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(32, 26);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 10;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 141F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(132, 427);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 174F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(177, 524);
             this.tableLayoutPanel2.TabIndex = 3;
             this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // richTextBox3
             // 
             this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox3.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox3.Location = new System.Drawing.Point(4, 4);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.ReadOnly = true;
-            this.richTextBox3.Size = new System.Drawing.Size(126, 22);
+            this.richTextBox3.Size = new System.Drawing.Size(169, 26);
             this.richTextBox3.TabIndex = 0;
             this.richTextBox3.Text = "";
             this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 31);
+            this.button1.Location = new System.Drawing.Point(4, 38);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 22);
+            this.button1.Size = new System.Drawing.Size(168, 26);
             this.button1.TabIndex = 1;
             this.button1.Text = "Color";
             this.button1.UseVisualStyleBackColor = true;
@@ -146,33 +152,21 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 59);
+            this.button2.Location = new System.Drawing.Point(4, 72);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.Size = new System.Drawing.Size(168, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(24, 453);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 28);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Reset All";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // colorDialog1
-            // 
-            this.colorDialog1.AllowFullOpen = false;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(3, 184);
+            this.button4.Location = new System.Drawing.Point(4, 226);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 9;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
@@ -181,20 +175,33 @@
             // button5
             // 
             this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button5.Location = new System.Drawing.Point(3, 325);
+            this.button5.Location = new System.Drawing.Point(4, 400);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(126, 30);
+            this.button5.Size = new System.Drawing.Size(169, 36);
             this.button5.TabIndex = 10;
             this.button5.Text = "Select File";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 558);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // button6
             // 
             this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button6.Location = new System.Drawing.Point(502, 453);
+            this.button6.Location = new System.Drawing.Point(670, 558);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 28);
+            this.button6.Size = new System.Drawing.Size(100, 34);
             this.button6.TabIndex = 4;
             this.button6.Text = "Lock";
             this.button6.UseVisualStyleBackColor = true;
@@ -203,21 +210,27 @@
             // button7
             // 
             this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button7.Location = new System.Drawing.Point(940, 453);
+            this.button7.Location = new System.Drawing.Point(1254, 558);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 28);
+            this.button7.Size = new System.Drawing.Size(100, 34);
             this.button7.TabIndex = 5;
             this.button7.Text = "Lock";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AllowFullOpen = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(1040, 505);
+            this.ClientSize = new System.Drawing.Size(1387, 622);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Parseur v1/Parseur v1/Parseur.cs
+++ Parseur v1/Parseur v1/Parseur.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Parseur_v1
 {
@@ -12,26 +14,59 @@ namespace Parseur_v1
     {
         private string[] separators;
         private string patternFinal;
+        private int nombreVar;
+        string filePath = @"C:\Users\dorba\Desktop\ParseurFile";
 
         public Parseur(string[] patternInitial, string patternFinal)
         {
             this.separators = patternInitial;
             this.patternFinal = patternFinal;
+            nombreVar = Regex.Matches(patternFinal, "{[0-9]+}").Count;
         }
 
+        /// <summary>
+        /// permet de load un parseur a l'aide du fichier contenant tout les parseurs sauvegardé
+        /// </summary>
+        /// <param name="parseurName"></param>
+        /// <returns></returns>
         public static Parseur LoadParseur(string parseurName)
         {
             return null;
         }
 
+        /// <summary>
+        /// permet de sauvegarder le parseur dans le fichier qui contient tout les parseur
+        /// </summary>
+        /// <remarks>
+        /// fichier sous la forme
+        /// <name>name</name>
+        /// <stringInitial>stringInitial</stringInitial>
+        /// <stringFinal>stringFinal</stringFinal>
+        /// </remarks>
+        public void SaveParseur()
+        {
+            StreamWriter sw = new StreamWriter(this.filePath);
+            sw.WriteLine("coucou");
+            sw.Close();
+        }
+
+        /// <summary>
+        /// créer un nouveau Parseur au donnée envoyé par la parti graphique
+        /// </summary>
+        /// <param name="initial"></param>
+        /// <param name="elementsInitial"></param>
+        /// <param name="final"></param>
+        /// <param name="elementsFinal"></param>
+        /// <returns></returns>
         public static Parseur CreateNewParseur(string initial, Dictionary<Color, Tuple<int, string>> elementsInitial, string final, Dictionary<Color, Tuple<int, string>> elementsFinal)
         {
             string[] patternInit;
             string patternFinal = final;
 
-            patternInit = initial.Split(elementsInitial.AsEnumerable().Select(x => x.Value.Item2).ToArray(), StringSplitOptions.RemoveEmptyEntries);//.Where(x => x.Length > 0).ToArray();
+            patternInit = initial.Split(elementsInitial.AsEnumerable().Select(x => x.Value.Item2).ToArray(), StringSplitOptions.RemoveEmptyEntries).Distinct().ToArray();//.Where(x => x.Length > 0).ToArray();
             
-            //sous la forme index, length
+            //sous la forme <index, length>
+            //permet de connaitre le decalage provoqué par le remplacement des mots pas {X} dans patternFinal
             List<Tuple<int, int>> decalage = new List<Tuple<int, int>>();
 
             int i = 0;
@@ -40,50 +75,142 @@ namespace Parseur_v1
                 string coloredString = elementInitial.Value.Item2;
                 
                 //replace the current word by {X} (where X is a number) in patternFinal
-
                 if (elementsFinal.TryGetValue(elementInitial.Key, out Tuple<int, string> elementFinal))
                 {
                     //int indexInit = elementInitial.Value.Item1;
+                    int a = i.ToString().Length;
                     int indexFinal = elementFinal.Item1 - (from decal in decalage
                                                           where decal.Item1 < elementFinal.Item1
-                                                          select decal.Item2 - (2 + i.ToString().Length)).Sum();
+                                                          select decal.Item2).Sum();
 
-                    patternFinal = new string(patternFinal.Take(indexFinal).ToArray()) + "{" + i + "}" + new string(patternFinal.Skip(indexFinal + coloredString.Length).ToArray());
-                    decalage.Add(new Tuple<int, int>(elementFinal.Item1, coloredString.Length));
+                    patternFinal = new string(patternFinal.Take(indexFinal).ToArray()) + "{" + i + "}"
+                        + new string(patternFinal.Skip(indexFinal + coloredString.Length).ToArray());
+                    decalage.Add(new Tuple<int, int>(elementFinal.Item1, coloredString.Length -(2 + i.ToString().Length)));
                     i++;
                 }
             }
-            //Console.WriteLine("initial : " + patternInit);
-            //Console.WriteLine("final   : " + patternFinal);
-            return new Parseur(patternInit, patternFinal); ;
+            return new Parseur(patternInit, patternFinal);
         }
 
+        /// <summary>
+        /// parse une string a l'aide de ce parseur
+        /// </summary>
+        /// <param name="stringToParse"></param>
+        /// <returns></returns>
+        /// <error>quand il manque une information dans la chaine a parser, le programme plante (a cause du string.Format)</error>
         public string DoParsing(string stringToParse)
         {
+            string basePattern = patternFinal;
             //recupere tout les mots qui sont des variables
-            List < string > variables = new List<string>();
-            variables = stringToParse.Split(this.separators, StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length > 0).ToList();
+            List <string> variables = new List<string>();
+            variables = stringToParse.Split(this.separators, StringSplitOptions.None).ToList();
 
-            return string.Format(patternFinal, variables.ToArray()); // Regex.Replace(result, "<var[0-9]*>", "");
+            //enleve les vide en debut et en fin si il y a trop de variables (se sont alors des vides rajouté par erreur)
+            int count = variables.Count();
+            while (count % this.nombreVar > 0 && count > this.nombreVar)
+            {
+                variables.RemoveAt(variables.First() == "" ? 0 : count - 1);
+                count--;
+            }
+
+            //permet de gérer les cycles
+            for(int i = 1; i < Math.Ceiling((double)variables.Count / this.nombreVar); i++)
+            {
+                string[] newVarName = new string[nombreVar];
+                for (int j = 0; j < nombreVar; j++)
+                {
+                    newVarName[j] = "{" + (i * nombreVar + j) + "}";
+                }
+                patternFinal += "\n" + string.Format(basePattern, newVarName);
+            }
+
+            return string.Format(patternFinal, variables.ToArray()); // Regex.Replace(result, "{[0-9]*}", "");
         }
 
+        /// <summary>
+        /// permet de testé rapidement les fonctionCreateNewParseur et DoParsing
+        /// </summary>
         public static void Test()
         {
-            string initial= ",{0}mm;;soeur//toto";
+            int length = 10000;
+            var parseur = TestCreatePasseur(length);
+            parseur.TestParseur(length);
+            //parseur.TestSave();
+        }
+
+        private static Parseur TestCreatePasseur(int length = 2)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+            string smallInit = "{0}mm;;soeur//toto,";
+            string smallFinal = "soeur\n{0}mm\ntoto\n";
+            string initial = string.Join(smallInit, new string[length]);
+            string final = string.Join(smallFinal, new string[length]);
+
+            //StreamWriter sw = new StreamWriter(@"C:\Users\dorba\Desktop\blop.txt");
+            //sw.WriteLine(initial);
+            //sw.Close();
+            //Console.WriteLine("toto");
+
+            //string initial = "{0}mm;;soeur//toto,";
+            //string final = "soeur\n{0}mm\ntoto\n";
+
             Dictionary<Color, Tuple<int, string>> elementsInitial = new Dictionary<Color, Tuple<int, string>>();
-            elementsInitial.Add(Color.Black, new Tuple<int, string>(1, "{0}mm"));
-            elementsInitial.Add(Color.AliceBlue, new Tuple<int, string>(8, "soeur"));
-            elementsInitial.Add(Color.AntiqueWhite, new Tuple<int, string>(15, "toto"));
-
-            string final = "soeur\n{0}mm\ntoto";
             Dictionary<Color, Tuple<int, string>> elementsFinal = new Dictionary<Color, Tuple<int, string>>();
-            elementsFinal.Add(Color.Black, new Tuple<int, string>(6, "{0}mm"));
-            elementsFinal.Add(Color.AliceBlue, new Tuple<int, string>(0, "soeur"));
-            elementsFinal.Add(Color.AntiqueWhite, new Tuple<int, string>(12, "toto"));
 
-            var a = CreateNewParseur(initial, elementsInitial, final, elementsFinal);
-            //Parseur a = new Parseur(new string[]{ ",", ":", ";;", "ii" }, "{0}\n{1}\n{2}\n{3}");
-            Console.WriteLine(a.DoParsing(",maman;;soeur//papa"));
+            Tuple<int, int, int> col = new Tuple<int, int, int>(0, 0, 0);
+            for (int i = 0; i < length-1; i++)
+            {
+                elementsInitial.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(1 + i * smallInit.Length, "{0}mm"));
+                elementsFinal.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(6 + i * smallFinal.Length, "{0}mm"));
+                col = addForColor(col);
+
+                elementsInitial.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(8 + i * smallInit.Length, "soeur"));
+                elementsFinal.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(0 + i * smallFinal.Length, "soeur"));
+                col = addForColor(col);
+
+                elementsInitial.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(15 + i * smallInit.Length, "toto"));
+                elementsFinal.Add(Color.FromArgb(col.Item1, col.Item2, col.Item3), new Tuple<int, string>(12 + i * smallFinal.Length, "toto"));
+                col = addForColor(col);
+            }
+            t.Stop();
+            Console.WriteLine("creation time : " + t.Elapsed);
+
+            Stopwatch timerCreateParseur = new Stopwatch();
+            timerCreateParseur.Start();
+            Parseur parseur = CreateNewParseur(initial, elementsInitial, final, elementsFinal);
+            timerCreateParseur.Stop();
+            Console.WriteLine("time to create the parsor : " + timerCreateParseur.Elapsed);
+            return parseur;
+        }
+
+        private static Tuple<int,int,int> addForColor(Tuple<int,int,int> col)
+        {
+            int a = col.Item1;
+            int b = col.Item2;
+            int c = col.Item3;
+
+            a++;
+            b += a / 256;
+            c += b / 256;
+
+            
+            return new Tuple<int, int, int>(a%256, b%256, c%256);
+        }
+
+        private void TestParseur(int length = 2)
+        {
+            Stopwatch timerParsing = new Stopwatch();
+            timerParsing.Start();
+            var result = this.DoParsing(string.Join("maman;;soeur//papa,", new string[length]));
+            timerParsing.Stop();
+            //Console.WriteLine(result);
+            Console.WriteLine("timer to parse : " + timerParsing.Elapsed);
+        }
+
+        private void TestSave()
+        {
+            this.SaveParseur();
         }
     }
 }
